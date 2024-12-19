794da549dd5410b3cf7f1f5cd56a7e1d1cc53d18
blaineoc <131784206+blaineoc@users.noreply.github.com>
2024-12-19 15:38:29 +0000

    Update members.php

--- members.php
+++ members.php
@@ -1,1 +1,13 @@
+table {
+    margin: 20px 0;
+    border: 1px solid #ddd;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 12px;
+    text-align: left;
+}
+td {
+    background-color: #f9f9f9;
+}
 
